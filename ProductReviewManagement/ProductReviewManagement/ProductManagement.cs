8d7f54fed84baabb869a415ef8bd1608fc627588
tutorial6 <manapure.vaibhav542@gmail.com>
2022-07-30 18:16:06 +0530

    UC5-Retrieve only Product Id and Review

--- ProductReviewManagement/ProductReviewManagement/ProductManagement.cs
+++ ProductReviewManagement/ProductReviewManagement/ProductManagement.cs
@@ -50,5 +50,18 @@ namespace ProductReviewManagement
                                     "\nReview Count : " + pm.count);
             }
         }
+
+        //UC5-Retrieve only Product Id and Review 
+        public static void RetrieveOnlyProductIdAndReview(List<ProductReview> Product)
+        {
+            var result = (from PM in Product orderby PM.ProductId select PM).ToList();
+            Console.WriteLine("\nRetrieve only Product Id and Review :");
+            foreach (var pm in result)
+            {
+                Console.WriteLine("\nProduct ID: " + pm.ProductId +
+                                    "\nReview : " + pm.Review);
+            }
+
+        }
     }
 }
